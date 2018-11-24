50bfd06e651e4aa1ac3f7440851b8d33a2300333
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2018-11-24 13:04:07 -0600

    Last indentation warnings

--- src/main/js/Pages/addPetPage.js
+++ src/main/js/Pages/addPetPage.js
@@ -4,32 +4,32 @@ import {RegistrationPetForm} from 'js/petModules/pet';
 import {Logout} from 'js/profileModules/logoutHelpers';
 
 export class AddPet extends React.Component {
-    render() {
-        return (
-            <div>
-            <div className="container shiftRight top-buffer">
-                <NavBar/>
-                <SideBar/>
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item">
-                        <a href="#/sits">Add Pet</a>
-                    </li>
-                </ol>
-                <div className="card mb-3">
-                    <div className="card-header">
-                        <i className="fas fa-table"></i>
-                        Add Pet
-                    </div>
-                    <div className='pl-3 pr-3 pt-3'>
-                    <RegistrationPetForm/>
-                    </div>
+	render() {
+		return (
+			<div>
+			<div className="container shiftRight top-buffer">
+				<NavBar/>
+				<SideBar/>
+				<ol className="breadcrumb">
+					<li className="breadcrumb-item">
+						<a href="#/sits">Add Pet</a>
+					</li>
+				</ol>
+				<div className="card mb-3">
+					<div className="card-header">
+						<i className="fas fa-table"></i>
+						Add Pet
+					</div>
+					<div className='pl-3 pr-3 pt-3'>
+					<RegistrationPetForm/>
+					</div>
 
-                </div>
-                <Logout/>
+				</div>
+				<Logout/>
 
-            </div>
-                <Footer/>
-            </div>
-        );
-    }
+			</div>
+				<Footer/>
+			</div>
+		);
+	}
 }
