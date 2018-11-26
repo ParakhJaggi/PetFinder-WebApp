3e1ad47d1cd9e7674a12dd6cb2eb59afd75d0d9a
Ian Laird <ian_laird@baylor.edu>
2018-11-25 19:07:57 -0600

    minor changes of backend layout

--- src/main/java/petfinder/site/common/pet/PetService.java
+++ src/main/java/petfinder/site/common/pet/PetService.java
@@ -1,8 +1,8 @@
 package petfinder.site.common.pet;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Service;
 import petfinder.site.common.Exceptions.PetException;
 import petfinder.site.common.RestRequests.AnimalTypeRequest;
 import petfinder.site.common.RestRequests.AnimalTypeRequestBuilder;
+import petfinder.site.common.user.BookingDTO;
+import petfinder.site.common.user.UserAuthenticationDto;
+import petfinder.site.common.user.UserDto;
+import petfinder.site.common.user.UserService;
 import petfinder.site.endpoint.PetEndpoint;
 
 /**
@@ -32,7 +36,23 @@ public class PetService {
 	}
 
 	public void save(PetDto pet) {
-		petDao.save(pet);
+		List<Object> userName = new ArrayList<>();
+		userName.add(SecurityContextHolder.getContext().getAuthentication().getName());
+		PetCollectionDTO pdo = findByOwner(userName);
+
+		long petID = new Random().nextLong();
+		if (petID < 0)
+			petID *= -1;
+		pet.setId(petID);
+		pet.setOwner(SecurityContextHolder.getContext().getAuthentication().getName());
+
+		//make sure that the owner does not double list an animal
+		if(pdo.getPets().stream().noneMatch(x  ->
+				x.getName().equals(pet.getName()) &&
+						x.getType().equals(pet.getType()) &&
+						x.getSubtype().equals(pet.getSubtype()))){
+			petDao.save(pet);
+		}
 	}
 
 	public void update(PetDto p){
@@ -43,6 +63,24 @@ public class PetService {
 			oldVersion.setPreferences(oldVersion.getPreferences());
 			petDao.save(p);
 		}
+	}
+
+	public PetCollectionDTO getSitPets(UserService us){
+		Set<PetDto> setOfPets = new HashSet<>();
+		PetCollectionDTO toReturn = new PetCollectionDTO();
+		String principal = SecurityContextHolder.getContext().getAuthentication().getName();
+		Optional<UserDto> retrieved = us.findUserByPrincipal(principal);
+		if(!retrieved.isPresent() || retrieved.get().getType() == UserDto.UserType.OWNER){
+			return null;
+		}
+		UserDto user = retrieved.get();
+		for(BookingDTO b : user.getBookings()){
+			List<Object> toMatch = new ArrayList<>();
+			toMatch.add(b.getPrincipal());
+			setOfPets.addAll((findByOwner(toMatch)).getPets());
+		}
+		toReturn.setPets(setOfPets.stream().collect(Collectors.toList()));
+		return toReturn;
 	}
 
 	/**
